78bb323beb08c7695508a06eb0e2abe65434a0ba
barlowdavid <barlow228@gmail.com>
2020-11-08 20:53:03 +0000

    15 - Spawning Units
    SampleScene:
    - Added Input EventSystem to the scene. So the IPointerClickHandler interface used in the UnitSpawner C# script functions properly.
    - Modified NetworkManager object. RTSNetworkManager now uses the new Player (prefab) for Player Prefab. Unit Spawner Prefab field set to UnitSpawner (prefab). UnitSpawner (prefab) also added to the list of Registered Spawnable Prefabs.
    - Modified Main Camera game object. Added Physics Raycaster component, so it can detect where mouse clicks relate to in the game world.
    
    Player Prefab:
    - New prefab which now represents the player. Added NetworkIdentity component.
    
    Unit Prefab:
    - Used to be the Player Prefab, but now re-named to Unit.
    
    UnitSpawner Prefab:
    - New game object set to a prefab. Added Cube mesh component as a child (which uses UnitSpawner_Mat as a material), UnitSpawnPoint transform as a child, NetworkIdentity component, Box Collider component, and Unit Spawner C# Script component. This script has Unit Prefab set to the Unit (prefab) and UnitSpawnPoint set to the child UnitSpawnPoint's transform.
    
    UnitSpawner_Mat:
    - New material which is the colour red. Used for the UnitSpawner prefab.
    
    RTSNetworkManager C# Script
    - Added private GameObject field called unitSpawnerPrefab set to a default of null. Serializable in the Editor.
    - Added public override void OnServerAddPlayer (NetworkConnection conn) method. It calls the same method firstly from its parent. Then it spawns an instance of unitSpawnerPrefab stored as a variable called unitSpawnerInstance. It spawns unitSpawnerInstance onto the network and sets the connecting player as the owner of the unitSpawnerInstance. This all happens when a player connects to the server.
    
    UnitSpawner C# Script:
    - New class derived from NetworkBehaviour and using IPointerClickHandler interface.
    - Added serializable private Gameobject field called UnitPrefab. Set to a default of null.
    - Added serializable private Transform field called UnitSpawnPoint. Set to a default of null.
    - Added private void CmdSpawnUnit() method, marked with the Command tag. It spawns the GameObject stored in the unitPrefab field into the world, at the field's unitSpawnPoint transform. The unitPrefab spawned is stored in a local variable called unitInstance. Then it spawns this unitInstance onto the network, giving the player who owns the spawner, ownership of the unitInstance.
    - Added public void OnPointerClick (PointerEventData eventData) method. If the eventData tells us that the button clicked is not the left mouse button, then exit the method. Then check that the client owns the UnitSpawner object it is clicking on. If it does not, exit the method. If it does, then the method will continue and call the CmdSpawnUnit() method.

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Transform unitSpawnPoint = null;
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate( // spawn prefab on the server
+            unitPrefab, 
+            unitSpawnPoint.position, 
+            unitSpawnPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient); // spawn unit on the network so clients receive, and make the client who owns the spawner the owner.
+    }
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button != PointerEventData.InputButton.Left) { return; } // if mouse button not clicked, exit method
+
+        if(!hasAuthority) { return; } // if client does not own spawner, exit method
+
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
-    
+    [SerializeField] private GameObject unitSpawnerPrefab = null;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate( // spawn a unitSpawner on the server.
+            unitSpawnerPrefab, 
+            conn.identity.transform.position, 
+            conn.identity.transform.rotation);
+
+        NetworkServer.Spawn(unitSpawnerInstance, conn); // spawn a unitSpawnerInstance on all the clients, and then give ownership to the player who was added.
+    }
 }
