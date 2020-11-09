b592f74b7ad4a55c619b0514ab7f94f643d0d009
barlowdavid <barlow228@gmail.com>
2020-11-09 11:03:31 +0000

    17 - Commanding Units
    SampleScene:
    - Renamed UnitSelectionHandler game object to UnitHandlers. Also added UnitCommandSender C# script component. Set Unit Selection Handler field to game object's own Unit Selection Handler C# script component. Layer Mask field set to Default.
    
    Unit Prefab:
    - Modified Unit C# Script component. Set Unit Movement field to Unit's own Unit Movement C# Script component.
    
    Unit C# Script:
    - Added new serializable private UnitMovement field called unitMovement. Set to null by default.
    - Added public UnitMovement GetUnitMovement() method. Returns the unitMovement field.
    
    UnitMovement C# Script:
    - Removed UnityEngine.InputSystem library.
    - Removed private Camera mainCamera field.
    - Removed public override void OnStartAuthority() method.
    - Removed private void Update() method.
    - Modified void CmdMove (Vector3 position) method from private to public.
    
    UnitSelectionHandler C# Script:
    - Modified List<Unit> SelectedUnits field. Now can be accessed via a getter.
    - Changed anywhere "selectedUnits" was in the code, to the now correct "SelectedUnits" spelling.
    
    UnitCommandSender C# Script:
    - New class.
    - Added serializable private UnitSelectionHandler field called unitSelectionHandler. Set to a default of null.
    - Added serializable private LayerMask field called layerMask. Set to a default of new LayerMask().
    - Added private Camera field called mainCamera. Set to Camera.main in the private void Start() method.
    - private void Update() method. Checks if the right mouse button has not been pressed, in which case it will exit the method. If it has been pressed then it will do a raytrace from the main camera to the mouse's current position. It will then check if this raycast hit anything on the default layer mask; if it hasn't it will exit the method. If it has, then the TryMove method will be called, with hit.point passed in.
    - Added private void TryMove (Vector3 point) method. Iterates over all of the units in unitSelectionHandler's SelectedUnits list field. Each unit has its GetUnitMovement() method called, then chained into CmdMove method with the parameter point passed in.

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Mirror;
+﻿using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Unit : NetworkBehaviour
 {
+    [SerializeField] private UnitMovement unitMovement = null;
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
+
+    public UnitMovement GetUnitMovement()
+    {
+        return unitMovement;
+    }
 
     #region Client
 
--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,7 +8,7 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private Camera mainCamera;
 
-    private List<Unit> selectedUnits = new List<Unit>();
+    public List<Unit> SelectedUnits { get; } = new List<Unit>();
 
     private void Start()
     {
@@ -22,12 +20,12 @@ public class UnitSelectionHandler : MonoBehaviour
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             
-           foreach (Unit selectedUnit in selectedUnits)
+           foreach (Unit selectedUnit in SelectedUnits)
            {
                 selectedUnit.Deselect();
            }
 
-           selectedUnits.Clear();
+           SelectedUnits.Clear();
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
@@ -45,9 +43,9 @@ public class UnitSelectionHandler : MonoBehaviour
 
         if (!unit.hasAuthority) { return; } // if unit is not owned by the player, exit method
 
-        selectedUnits.Add(unit);
+        SelectedUnits.Add(unit);
 
-        foreach(Unit selectedUnit in selectedUnits)
+        foreach(Unit selectedUnit in SelectedUnits)
         {
             selectedUnit.Select();
         }
