8b2fade0358cc97306462bbb12d6bb720f71a815
barlowdavid <barlow228@gmail.com>
2020-11-10 15:58:51 +0000

    18 - Storing Our Units
    Player Prefab:
    - Added RTSPlayer C# Script component.
    
    Unit C# Script:
    - Added System include so Action keyword accessible.
    - Added public static event Action<Unit> ServerOnUnitSpawned. An event for when a unit is spawned - dealing with server logic.
    - Added public static event Action<Unit> ServerOnUnitDespawned. An event for a when a unit is despawned - dealing with server logic.
    - Added public static event Action<Unit> AuthorityOnUnitSpawned. An event for when a unit is spawned - dealing with client (owner) logic.
    - Added public static event Action<Unit> AuthorityOnUnitDespawned. An event for a when a unit is despawned - dealing with client (owner) logic.
    - Added public override void OnStartServer method. Calls the ServerOnUnitSpawned event.
    - Added public override void OnStopServer method. Calls the ServerOnUnitDespawned event.
    - Added public override void OnStartClient method. Calls the AuthorityOnUnitSpawned event, if it is a client and the client is the owner.
    - Added public override void OnStopClient method. Calls the AuthorityOnUnitDespawned event, if it is a client and the client is the owner.
    
    RTSPlayer C# Script:
    - New class derived from Mirror's NetworkBehaviour.
    - New serializable (intended not to change but just for debugging) private List<Unit> field called myUnits. Set to a default of new List<Unit>()
    - Added public override void OnStartServer method. Subscribes the unit's ServerOnUnitSpawned event to the ServerHandleUnitSpawned method inside this class. Also subscribes the unit's ServerOnUnitDespawned event to the ServerHandleUnitDespawned method inside this class.
    - Added public override void OnStopServer method. Unsubscribes the unit's ServerOnUnitSpawned event from the ServerHandleUnitSpawned method inside this class. Also unsubscribes the unit's ServerOnUnitDespawned event from the ServerHandleUnitDespawned method inside this class.
    - Added private void ServerHandleUnitSpawned (Unit unit) method. First of all checks if the unit's owner is not equal to the client's connectionId, in which case it will exit the method. If this is false then it will add the parameter unit to the myUnits list.
    - Added private void ServerHandleUnitDespawned (Unit unit) method. First of all checks if the unit's owner is not equal to the client's connectionId, in which case it will exit the method. If this is false then it will remove the matching parameter unit from the myUnits list.

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    [SerializeField] private List<Unit> myUnits = new List<Unit>();
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned; // subscribe unit's ServerOnUnitSpawned event to ServerHandleUnitSpawned method.
+        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned; // subscribe unit's ServerOnUnitDespawned event to ServerHandleUnitDespawned method.
+    }
+
+    public override void OnStopServer()
+    {
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned; // unsubscribe unit's ServerOnUnitSpawned event from ServerHandleUnitSpawned method.
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned; // unsubscribe unit's ServerOnUnitDespawned event from ServerHandleUnitDespawned method.
+    }
+
+    private void ServerHandleUnitSpawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; } // if unit's owner is not same as the client, exit method.
+
+        myUnits.Add(unit);
+    }
+
+    private void ServerHandleUnitDespawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly) { return; }
+
+        Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned; 
+        Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly) { return; }
+
+        Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned; 
+        Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned; 
+    }
+
+    private void AuthorityHandleUnitSpawned(Unit unit)
+    {
+        if (!hasAuthority) { return; } // if client does not own unit, exit method.
+
+        myUnits.Add(unit);
+    }
+
+    private void AuthorityHandleUnitDespawned(Unit unit)
+    {
+        if (!hasAuthority) { return; }
+
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+
+
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,4 +1,5 @@
-﻿using Mirror;
+﻿using System;
+using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,12 +9,46 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned; // Server event for when a unit is spawned. 
+    public static event Action<Unit> ServerOnUnitDespawned; // Server event for when a unit is despawned.
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
 
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
+
     #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; } // if the server or not the owner, exit method.
+
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
 
     [Client]
     public void Select()
