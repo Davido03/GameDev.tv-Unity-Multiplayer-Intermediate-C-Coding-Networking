8dc25f48ddec7133492b4c562564d29b54111474
barlowdavid <barlow228@gmail.com>
2020-11-11 08:12:43 +0000

    19 - Selecting Multiple Units
    Scene_Map_01:
    - Renamed SampleScene to Scene_Map_01. All related assets now prefixed with Scene_Map_01 too.
    - UnitHandlers game object now has an UI Image child game object, called DragBox. By default its SetActive is set to false. Its anchor starts from the bottom left. While the colour of the image is set to black, with no source image set. Position, rotation and width\height are all set to 0.
    - UnitHandlers game object's UnitSelectionHandler C# script component's UnitSelectionArea field set to DragBox (child of UnitHandlers).
    
    Scene_Menu:
    - Created new scene to be used as the main menu.
    -  Added an instance of the NetworkManager prefab to the scene.
    - Set Offline Scene to Scene_Menu.
    - Set Online Scene to Scene_Map_01.
    
    NetworkManager Prefab:
    - Created a prefab of the NetworkManager that deals with all the networking.
    
    RTSPlayer C# Script:
    - Added new public List<Unit> GetMyUnits() method. Returns the myUnits field.
    
    UnitSelectionHandler C# Script:
    - Added Mirror as an include.
    - Added serializable private RectTransform field called unitSelectionArea. Set to a default of null.
    - Added private Vector2 field called startPosition.
    - Added private RTSPlayer field called player.
    - Modified update method. If the player field is set to null, it will attempt to set the player field to the client's RTSPlayer component. If the mouse left button has been pressed this frame, it will call the StartSelectionArea() method. Otherwise if the mouse left button is being pressed down continously, it will call the UpdateSelectionArea() method.
    - Added private void StartSelectionArea() method. Firstly it will cycle over all of the units in the SelectedUnits list field and deselect them. Then it will clear the SelectedUnits list field. The unitSelectionArea game object will be set active. Mouse's current position is stored in the startPosition field. Lastly the UpdateSelectionArea() method is called.
    - Added private void UpdateSelectionArea() method. It reads the mouse position in and then uses this information, along with the startPosition field, to calculate the areaWidth and areaHeight of the selection area. This is then used to create the sizeDelta of the unitSelectionArea and its anchored position.
    - Modified private void ClearSelectionArea() method. Sets the unitSelectionArea game object's set active to false. Then if the size of the unit selection area is 0 it will do code to only select one unit. Exiting the method after this is done. If the size is not 0 though, it will allow for the unit selection area box to be expanded or contracted. Then it will iterate over all of the player's units and see which are within the selection area box. Those which are will be added to the SelectedUnits list field and the Select() method called on each individual unit, in the box.
    
    EditorBuildSettings:
    - SampleScene replaced with Scene_Map_01 scene to scenes to build.
    - Added Scene_Menu to scenes to build.

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class UnitSelectionHandler : MonoBehaviour
 {
+    [SerializeField] private RectTransform unitSelectionArea = null;
+
     [SerializeField] private LayerMask layerMask = new LayerMask();
 
+    private Vector2 startPosition;
+
+    private RTSPlayer player;
     private Camera mainCamera;
 
     public List<Unit> SelectedUnits { get; } = new List<Unit>();
@@ -17,37 +23,91 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private void Update()
     {
+        if (player == null)
+        {
+            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>(); // will give error for two frames - fix in later lecture
+        }
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            
-           foreach (Unit selectedUnit in SelectedUnits)
-           {
-                selectedUnit.Deselect();
-           }
-
-           SelectedUnits.Clear();
+            StartSelectionArea();
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
             ClearSelectionArea();
         }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+            UpdateSelectionArea();
+        }
+    }
+
+    private void StartSelectionArea()
+    {
+        foreach (Unit selectedUnit in SelectedUnits)
+        {
+            selectedUnit.Deselect();
+        }
+
+        SelectedUnits.Clear();
+
+        unitSelectionArea.gameObject.SetActive(true);
+
+        startPosition = Mouse.current.position.ReadValue();
+
+        UpdateSelectionArea();
+    }
+
+    private void UpdateSelectionArea()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        float areaWidth = mousePosition.x - startPosition.x;
+        float areaHeight = mousePosition.y - startPosition.y;
+
+        unitSelectionArea.sizeDelta = new Vector2(Mathf.Abs(areaWidth), Mathf.Abs(areaHeight));
+        unitSelectionArea.anchoredPosition = startPosition + new Vector2(areaWidth / 2, areaHeight / 2);
     }
 
     private void ClearSelectionArea()
     {
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        unitSelectionArea.gameObject.SetActive(false);
 
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; } // if raycast hits nothing, exit method
-
-        if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; } // if gameobject we hit is not a unit, exit method
-
-        if (!unit.hasAuthority) { return; } // if unit is not owned by the player, exit method
-
-        SelectedUnits.Add(unit);
-
-        foreach(Unit selectedUnit in SelectedUnits)
+        // if size of selection area is 0, only check for selecting one unit
+        if (unitSelectionArea.sizeDelta.magnitude == 0)
         {
-            selectedUnit.Select();
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; } // if raycast hits nothing, exit method
+
+            if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; } // if gameobject we hit is not a unit, exit method
+
+            if (!unit.hasAuthority) { return; } // if unit is not owned by the player, exit method
+
+            SelectedUnits.Add(unit);
+
+            foreach (Unit selectedUnit in SelectedUnits)
+            {
+                selectedUnit.Select();
+            }
+
+            return;
+        }
+
+        // multi-unit selection area check
+        Vector2 min = unitSelectionArea.anchoredPosition - (unitSelectionArea.sizeDelta / 2);
+        Vector2 max = unitSelectionArea.anchoredPosition + (unitSelectionArea.sizeDelta / 2);
+
+        foreach(Unit unit in player.GetMyUnits()) 
+        {
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(unit.transform.position); // get screen position of unit
+
+            if (screenPosition.x > min.x && screenPosition.x < max.x && // if screen position of unit is within selection box
+                screenPosition.y > min.y && screenPosition.y < max.y)
+            {
+                SelectedUnits.Add(unit); // add unit to list of selected units
+                unit.Select(); // select unit
+            }
         }
     }
 }
