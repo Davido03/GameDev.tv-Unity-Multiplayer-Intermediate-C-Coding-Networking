1c598cfb9811d8c62807c8e7cc90e63e90d2d45a
barlowdavid <barlow228@gmail.com>
2020-11-12 19:38:11 +0000

    24 - Dealing Damage
    Unit Prefab:
    - Added Health C# script component.
    
    UnitSpawner Prefab:
    - Added Health C# script component.
    
    UnitProjectile C# Script:
    - Added serializable private int field called damageToDeal. Set to a default of 20.
    - Added private void OnTriggerEnter (Collider other) method, tagged with [ServerCallback]. When another object colliders with the trigger of the owning object of the script it will do the following: check if the colliding object has a NetworkIdentity component. If it does then it will check if the owner of the colliding object is equal to the owner of the object of this script. If true it will exit the method. Then it checks if the colliding object has a Health component, which if it does it will call the DealDamage method on the Health component. Passing in the damageToDeal field. After all of this it will then call the DestroySelf() method.
    
    Health C# Script:
    - New class derived from Mirror's NetworkBehaviour class.
    - Added serializable private int field called maxHealth. Set to a default of 100.
    - Added private int field called currentHealth. Tagged with [SyncVar].
    - Added public event Action ServerOnDie. An event that triggers when a unit reaches 0 health and dies.
    - Added public override void OnStartServer() method. Sets the currentHealth field to the value of the maxHealth field.
    - Added public void DealDamage (int damageAmount) method, tagged with [Server]. If the currentHealth field is at 0 then it will exit the method immediately. Otherwise it will set currentHealth to the larger value of currentHealth - damageAmount or 0. This prevents currentHealth from being set to a negative value. Then it will check if the currentHealth is not equal to 0, which if true will cause the method to be exited. Otherwise it will call the ServerOnDie event, then log to the console the string, "We Died!".

--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
 
@@ -17,6 +18,21 @@ public class UnitProjectile : NetworkBehaviour
     public override void OnStartServer()
     {
         Invoke(nameof(DestroySelf), destroyAfterSeconds); // call method after destroyAfterSeconds (5f by default)
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity)) // if object we hit has a NetworkIdentity component
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) { return; } // if connectionToClient matches connectionToClient (hit own unit) then exit method
+        }
+        if (other.TryGetComponent<Health>(out Health health)) // if object we have hit has a health component
+        {
+            health.DealDamage(damageToDeal);
+        }
+
+        DestroySelf();
     }
 
     [Server]
