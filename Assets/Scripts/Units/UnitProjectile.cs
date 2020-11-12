755478d988352328deadbbb2baaafb1dbfd6ce7c
barlowdavid <barlow228@gmail.com>
2020-11-12 17:40:01 +0000

    23 - Unit Firing
    NetworkManager Prefab:
    - Added Projectile Prefab to list of Registered Spawnable Prefabs.
    
    Unit Prefab:
    - Added ProjectileSpawnPoint child game object. Position set to 0, 1.65, 2.7.
    - Added UnitFiring C# script component. Targeter field set to the Targeter C# script component on the prefab. Projectile Prefab field set to the Projectile (Prefab). Projectile Spawn Point field set to the Projectile Spawn Point child game object. Fire Range field set from the default of 10 to 11. Rotation Speed field set from the default of 20 to 180.
    
    Projectile Prefab:
    - New prefab. Scale set to 0.75, 0.75, 0.75.
    - Added NetworkIdentity component.
    - Added Sphere Mesh Renderer with Sphere Collider. Sphere Collider set to IsTrigger = true.
    - Added Rigidbody component. Angular drag set to 0 and Use Gravity set to false.
    - Added Unit Projectile C# script component. rb field set to the Rigidbody on the prefab.
    
    UnitFiring C# Script:
    - New class derived from NetworkBehaviour.
    - Added serializable private Targeter field called targeter. Set to a default of null.
    - Added serializable private GameObject field called projectilePrefab. Set to a default of null.
    - Added serializable private Transform field called projectileSpawnPoint. Set to a default of null.
    - Added serializable private float field called fireRange. Set to a default of 5f.
    - Added serializable private float field called fireRate. Set to a default of 1f.
    - Added serializable private float field called rotationSpeed. Set to a default of 20f.
    - Added private float field called lastFireTime.
    - Added private void Update() method with [ServerCallback] tag. Cache targeter.GetTarget() into Targetable target local variable. Then check whether there is not a target, or if CanFireAtTarget() method returns false. If either are the case then exit the method. Otherwise cache the rotation difference between the target's position and the unit's position into the targetRotation local variable. Then rotate the unit each frame between the unit's rotation and target rotation, using the rotationSpeed field. Lastly it then checks if the time has been greater since what the fireRate allows and the lastFireTime. If this is true it caches the rotational difference between the position of the target's AimAtPoint child object and the projectileSpawnPoint field's position. Then it instantiates the object in the projectilePrefab field at the position of the object in the projectileSpawnPoint field. Using the cached projectileRotation local variable for the rotation of the projectile. Then it spawns the projectileInstance onto the server as well, with the owner set to whoever owns the object calling the script. LastFireTime is set to Time.time.
    - Added private bool CanFireAtTarget() method with [Server] tag. Returns whether the target is in range of the unit's fire range, with true or false.
    
    UnitProjectile C# Script:
    - New class derived from NetworkBehaviour.
    - Added serializable private Rigidbody field called rb. Set to a default of null.
    - Added serializable private float field called destroyAfterSeconds. Set to a default of 5f.
    - Added serializable private float field called launchForce. Set to a default of 10f.
    - Added private void Start() method. Set the velocity of the Rigidbody rb field to be launchForce (10f by default) on the forward axis.
    - Added public override void OnStartServer() method. Calls the DestroySelf() method after destroyAfterSeconds (5f by default).
    - Added private void DestroySelf() with [Server] tagged. Destroys the gameObject carrying this script on the server.

---
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class UnitProjectile : NetworkBehaviour
+{
+    [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private float destroyAfterSeconds = 5f;
+    [SerializeField] private float launchForce = 10f;
+
+    private void Start()
+    {
+        rb.velocity = transform.forward * launchForce;
+    }
+
+    public override void OnStartServer()
+    {
+        Invoke(nameof(DestroySelf), destroyAfterSeconds); // call method after destroyAfterSeconds (5f by default)
+    }
+
+    [Server]
+    private void DestroySelf()
+    {
+        NetworkServer.Destroy(gameObject); // destroy this game object on server
+    }
+}
