44f0e1645fce0642dc9b27cf610385cb6b2d6ec7
barlowdavid <barlow228@gmail.com>
2020-11-13 06:49:21 +0000

    25 - Health UI
    Unit Prefab:
    - Added HealthBarCanvas prefab as a child game object. Rect Transform position set to 0, 2.75, 0, with width: 2 and height: 0.3.
    
    UnitSpawner Prefab:
    - Added HealthBarCanvas prefab as a child game object. Rect Transform position set to 0, 1.5, 0, with width: 1.5 and height: 0.2.
    
    HealthBarCanvas Prefab:
    - New prefab. A canvas with two UI Image child game objects. Canvas set to have a width of 1 and a height of 0.2. With all of the anchors set to 0.
    - HealthBar_Background is the first UI Image child game object. Set to stretch to fit the canvas.
    - HealthBar_Health is the second UI Image child game object. Set to stretch to fit the canvas. Has padding of 0.02 on all sides. Source Image set to Square and Color set to a dark red. Image Type set to Filled, Fill Method set to Horizontal, and Fill Origin set to Right.
    
    ProjectSettings:
    - Set Input Manager to support both the old and new systems.
    
    Health C# Script:
    - Modified private int currentHealth field. Is now tagged with [SyncVar(hook = nameof(HandleHealthUpdated))]. Whenever the field is updated call the HandleHealthUpdated() method.
    - Added public event Action<int, int> ClientOnHealthUpdated. An event that is called whenever the currentHealth field is updated.
    - Added private void HandleHealthUpdated (int oldHealth, int newHealth) method. Calls the ClientOnHealthUpdated event, with the input parameters: newHealth and maxHealth.
    
    HealthDisplay C# Script:
    - New class used to display a health bar.
    - Added serializable private Health field called health. Set to a default of null.
    - Added serializable private GameObject field called healthBarParent. Set to a default of null.
    - Added serializable private Image field called healthBarImage. Set to a default of null.
    - Added private void Awake() method. Subscribes the health's field ClientOnHealthUpdated event to the HandleHealthUpdated method.
    - Added private void OnDestroy() method. Unsubscribes the health's field ClientOnHealthUpdated event from the HandleHealthUpdated method.
    - Added private void OnMouseEnter() method. Sets the healthBarParent game object to true for SetActive.
    - Added private void OnMouseExit() method. Sets the healthBarParent game object to false for SetActive.
    - Added private void HandleHealthUpdated (int currentHealth, int maxHealth) method. Sets the fillAmount of the healthBarImage Image field to be (float)currentHealth / maxHealth (the two input parameters). Will be a float between 0-1.
    
    Square.png:
    - An image of a square. Created from Unity's in-built sprite system.

---
+++ Assets/Scripts/Combat/HealthDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] private Health health = null;
+    [SerializeField] private GameObject healthBarParent = null;
+    [SerializeField] private Image healthBarImage = null;
+
+    private void Awake()
+    {
+        health.ClientOnHealthUpdated += HandleHealthUpdated;
+    }
+
+    private void OnDestroy()
+    {
+        health.ClientOnHealthUpdated -= HandleHealthUpdated;
+    }
+
+    private void OnMouseEnter()
+    {
+        healthBarParent.SetActive(true); // turn on UI
+    }
+
+    private void OnMouseExit()
+    {
+        healthBarParent.SetActive(false); // turn off UI
+    }
+
+    private void HandleHealthUpdated(int currentHealth, int maxHealth)
+    {
+        healthBarImage.fillAmount = (float)currentHealth / maxHealth; // currentHealth converted to float so will give us a float and not an int value.
+    }
+}
