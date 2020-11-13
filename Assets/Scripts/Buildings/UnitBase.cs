c28511491ded26da0e24fc51ded9829ac74609fb
barlowdavid <barlow228@gmail.com>
2020-11-13 18:49:34 +0000

    27 - Unit Base Building
    NetworkManager Prefab:
    - Modified the RTSNetworkManager C# script component. Set the GameOverHandlerPrefab field to the GameOverHandler prefab.
    
    UnitBase Prefab:
    - Changed name from UnitSpawner to UnitBase.
    - Added UnitBase C# script component. Set Health field to the Health C# script component on the prefab.
    
    GameOverHandlerPrefab:
    - New prefab.
    - Added the NetworkIdentity component.
    - Added GameOverHandler C# script component.
    
    UnitSpawner C# Script:
    - Commented out NetworkServer.Destroy(gameObject) method call. So does not get called twice, as UnitBase C# Script implements this exact functionality too.
    
    RTSNetworkManager C# Script:
    - Added UnityEngine.SceneManagement module.
    - Added serializable private GameOverHandler field called gameOverHandlerPrefab. Set to a default of null.
    - Added public override void OnServerSceneChanged (string sceneName) method. If the current active scene begins with the string "Scene_Map", it will identify the scene as a game map. This will instantiate the gameOverHandlerPrefab and store it into the local variable gameOverHandlerInstance. Then the gameOverHandlerInstance game object will be spawned onto the server.
    
    GameOverHandler C# Script:
    - New class derived from Mirror's NetworkBehaviour class.
    - Added private List<UnitBase> field called bases. Set to create an empty new List of UnitBase.
    - Added public override void OnStartServer() method. Subscribes the UnitBase input parameter's ServerOnBaseSpawned event to the ServerHandleBaseSpawned method. Also subscribes the UnitBase input parameter's ServerOnBaseDespawned event to the ServerHandleBaseDespawned method.
    - Added public override void OnStopServer() method. Unsubscribes the UnitBase input parameter's ServerOnBaseSpawned event from the ServerHandleBaseSpawned method. Also unsubscribes the UnitBase input parameter's ServerOnBaseDespawned event from the ServerHandleBaseDespawned method.
    - Added private void ServerHandleBaseSpawned (UnitBase unitBase) method. Adds the unitBase input parameter to the bases list field.
    - Added private void ServerHandleBaseDespawned (UnitBase unitBase) method. Removes the unitBase input parameter from the bases list field. If the number of entries in the bases list field is not equal to 1 then exit the method. Otherwise log to the console, the string "Game Over!".
    
    UnitBase C# Script:
    - New class derived from Mirror's NetworkBehaviour class.
    - Added serializable private Health field called health. Set to a default of null.
    - Added public static event Action<UnitBase> called ServerOnBaseSpawned. An event for when the base is spawned onto the server.
    - Added public static event Action<UnitBase> called ServerOnBaseDespawned. An event for when the base is despawned from the server.
    - Added public override void OnStartServer() method. Subscribes the health field's ServerOnDie event to the ServerHandleDie method. Calls the ServerOnBaseSpawned event.
    - Added public override void OnStartServer() method. Unsubscribes the health field's ServerOnDie event from the ServerHandleDie method. Calls the ServerOnBaseDespawned event.
    - Added private void ServerHandleDie() method with the tag [Server]. Destroys the calling gameObject on the server.

---
+++ Assets/Scripts/Buildings/UnitBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class UnitBase : NetworkBehaviour
+{
+    [SerializeField] private Health health = null;
+
+    public static event Action<UnitBase> ServerOnBaseSpawned;
+    public static event Action<UnitBase> ServerOnBaseDespawned;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        health.ServerOnDie += ServerHandleDie;
+
+        ServerOnBaseSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        health.ServerOnDie -= ServerHandleDie;
+
+        ServerOnBaseDespawned?.Invoke(this);
+    }
+
+    [Server]
+    private void ServerHandleDie()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+
+    #endregion
+
+    #region Client
+
+    #endregion
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] private GameObject unitSpawnerPrefab = null;
+    [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
 
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
@@ -17,5 +19,15 @@ public class RTSNetworkManager : NetworkManager
             conn.identity.transform.rotation);
 
         NetworkServer.Spawn(unitSpawnerInstance, conn); // spawn a unitSpawnerInstance on all the clients, and then give ownership to the player who was added.
+    }
+
+    public override void OnServerSceneChanged(string sceneName) //whenever server changes the scene
+    {
+        if (SceneManager.GetActiveScene().name.StartsWith("Scene_Map"))
+        {
+            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+        }
     }
 }
