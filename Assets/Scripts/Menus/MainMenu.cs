e548a5a0c67e21b47e2694f6675d53568acff716
barlowdavid <barlow228@gmail.com>
2020-11-23 18:48:03 +0000

    47 - Using The Steam Transport
    Scene_Menu:
    - On MainMenuDisplay game object, modified MainMenu C# script component. Set useSteam bool field to true.
    
    NetworkManager Prefab:
    - Modified RTSNetworkManager C# script component. Set transport to NetworkManager (Fizzy Steamworks).
    - Kcp Transport component set to not be active.
    - Added Steam Manager C# script component.
    - Added Fizzy Steamworks C# script component.
    
    MainMenu C# Script:
    - Added serializable private bool field called useSteam. Set to a default of false.
    - Added protected Callback <LobbyCreated_t> called lobbyCreated.
    - Added protected Callback <LobbyCreated_t> called gameLobbyJoinRequested.
    - Added protected Callback <LobbyCreated_t> called lobbyEntered.
    - Added private void Start() method. If the useSteam bool field is set to false, then exit the method. Otherwise link the lobbyCreated callback with the OnLobbyCreated method. Then link the gameLobbyJoinRequested callback with the OnGameLobbyJoinRequested method. Finally link the lobbyEntered callback with the OnLobbyEntered method.
    - Modified public void HostLobby() method. If useSteam bool field is set to true, create a friends only lobby set to a maximum of 4 players. Then exit the method. Otherwise start hosting using the NetworkManager as usual.
    - Added private void OnLobbyCreated (LobbyCreated_t callback) method. If our callback result does not return a result of OK, set the landingPagePanel gameObject field to be active. Then exit the method. Otherwise set the lobby data for the Steam lobby using the callback's SteamIDLobby info, with the host's aaddress and the steam user's steamID.
    - Added private void OnGameLobbyJoinRequested (GameLobbyJoinRequested_t callback) method. Calls the JoinLobby method from Steamwork's SteamMatchmaking class, using the input parameter callback's steam ID lobby. Allowing a player to join the lobby.
    - Added private void OnLobbyEntered (LobbyEnter_t callback) method. If the host then exit the method. Otherwise set the hostAddress local string variable to be the result of SteamMatchMaking's GetLobbyData method. Using the callback's SteamIDLobby info and the host's address as the parameters. Then set NetworkManager's networkAddress to the hostAddress variable we just set. Then call NetworkManager's StartClient() method to connect. Finally set landingPagePanel gameObject field to not be active.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,16 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using Steamworks;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject landingPagePanel = null;
 
+    [SerializeField] private bool useSteam = false;
+
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
+
+    private void Start()
+    {
+        if (!useSteam) { return; }
+
+        // link steam's callbacks to our methods
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
+
     public void HostLobby()
     {
         landingPagePanel.SetActive(false);
 
+        if (useSteam)
+        {
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4); // friends only lobby, with max of 4 players
+            return;
+        }
+
         NetworkManager.singleton.StartHost();
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK) // if lobby not created successfully
+        {
+            landingPagePanel.SetActive(true);
+            return;
+        }
+
+        // if lobby created successfully
+
+        NetworkManager.singleton.StartHost();
+
+        SteamMatchmaking.SetLobbyData(
+        new CSteamID(callback.m_ulSteamIDLobby),
+        "HostAddress",
+        SteamUser.GetSteamID().ToString());
+    }
+
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby); // let player join lobby
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active) { return; } // if the host exit the method
+
+        // if not the host
+
+        string hostAddress = SteamMatchmaking.GetLobbyData( //get the address of host
+            new CSteamID(callback.m_ulSteamIDLobby),
+            "HostAddress");
+
+        NetworkManager.singleton.networkAddress = hostAddress; // set network address to that host address
+        NetworkManager.singleton.StartClient(); // connect to that host address
+
+        landingPagePanel.SetActive(false);
     }
 }
